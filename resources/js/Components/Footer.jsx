4be8349ec3aff65fb8f8a4b8fb8a5fa454632164
syafi-M <syafimq00@gmail.com>
2024-04-29 13:19:23 +0700

    SIDEBAR

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Footer(props) {
   return (
     <>
-      <footer className="footer fixed bottom-0 h-10 footer-center p-4 bg-gray-300 text-base-content">
+      <footer className="footer fixed bottom-0 z-20 h-10 footer-center p-4 bg-gray-300 text-base-content">
         <aside>
-          <p>
+          <p className="text-sm">
             Copyright © {new Date().getFullYear()} - All right reserved by PT.
             SAC Ponorogo Design And Code by Aditya & Syafi
           </p>
--- resources/js/Layouts/AdminLayout.jsx
+++ resources/js/Layouts/AdminLayout.jsx
@@ -1,19 +1,71 @@
 import Footer from "@/Components/Footer";
 import Sidebar from "@/Pages/Admin/Component/Sidebar";
-import React from "react";
-import { BiExtension } from "react-icons/bi";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useState } from "react";
+import { BiChevronsLeft, BiChevronsRight, BiExtension } from "react-icons/bi";
 import { ToastContainer } from "react-toastify";
 
 function AdminLayout({ children, overflow }) {
+  const [open, setOpen] = useState(true);
+  const openSideBar = () => {
+    setOpen(!open);
+  };
   return (
     <>
       <div
-        className={`flex ${overflow ? overflow : "overflow-auto"} bg-gray-200`}
+        className={`flex ${
+          overflow ? overflow : "overflow-auto"
+        } w-full bg-gray-200 gap-2`}
       >
-        <Sidebar link={"employes.index"} value={"Employes Data"}>
-          {<BiExtension />}
-        </Sidebar>
-        <div className=" w-screen p-10 min-h-screen">
+        <AnimatePresence>
+          <motion.div
+            initial={{ left: "0" }}
+            animate={{ left: 0, width: open ? "100%" : "0%" }}
+            transition={{ duration: 0.15, transition: { ease: "easeInOut" } }}
+            className={`flex items-center fixed z-10 `}
+          >
+            <motion.div
+              animate={{
+                opacity: open ? 1 : 0,
+
+                translateX: open ? "0%" : "-100%",
+              }}
+              transition={{
+                duration: 0.3,
+                transition: { ease: "easeIn" },
+              }}
+              className="relative"
+            >
+              <Sidebar
+                link={"employes.index"}
+                value={"Employes Data"}
+                open={open}
+              >
+                {<BiExtension />}
+              </Sidebar>
+            </motion.div>
+            <motion.div
+              animate={{
+                translateX: open ? "0%" : "-280%",
+              }}
+              transition={{
+                duration: 0.3,
+                transition: { ease: "easeInOut" },
+              }}
+              onClick={() => openSideBar()}
+              className="bg-orange-50  z-10 sm:hidden py-6 pl-2 pr-4 rounded-r-full text-center text-2xl"
+            >
+              <BiChevronsLeft
+                className={`transition-all duration-300 ${
+                  !open && "rotate-180"
+                }`}
+              />
+            </motion.div>
+          </motion.div>
+        </AnimatePresence>
+        <div
+          className={`w-full p-10 min-h-screen sm:ml-48 overflow-hidden sm:overflow-auto`}
+        >
           <ToastContainer
             position="top-center"
             autoClose={5000}
